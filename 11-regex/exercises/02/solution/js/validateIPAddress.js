93371f5580c7289a467172d97c5936dae9b9c14d
timivarga <timea.varga252@gmail.com>
2022-12-08 10:18:25 +0100

    kesz

--- 11-regex/exercises/02/solution/js/validateIPAddress.js
+++ 11-regex/exercises/02/solution/js/validateIPAddress.js
@@ -1,1 +1,11 @@
+/*2. Írj egy függvényt `validateIPAddress`, ami paraméterként egy IPv4 címet vár stringként!   
+A függvény egy `Boolean` értékben visszaadja, hogy a paraméterként megadott IP cím megfelelő formátumú e, vagy sem.
+*/
+
+const IPv4Regexp = /(^(0\.0\.0\.0)$|^(((([1-9]|[1-9]\d|1\d\d)|(2[0-4]\d)|(25[0-5]))\.)(((\d|[1-9]\d|1\d\d)|(2[0-4]\d)|(25[0-5]))\.){2}((\d|[1-9]\d|1\d\d)|(2[0-4]\d)|(25[0-5])))$)/;
+
+const validateIPAddress = (IPv4Number) => {
+    return IPv4Regexp.test(IPv4Number);
+}
+
 export default validateIPAddress;
--- 11-regex/exercises/04/solution/js/validate.js
+++ 11-regex/exercises/04/solution/js/validate.js
@@ -1,1 +1,11 @@
+const Roles = {
+    visa: /^[4]\d{15}$/,
+    ip: /(^(0\.0\.0\.0)$|^(((([1-9]|[1-9]\d|1\d\d)|(2[0-4]\d)|(25[0-5]))\.)(((\d|[1-9]\d|1\d\d)|(2[0-4]\d)|(25[0-5]))\.){2}((\d|[1-9]\d|1\d\d)|(2[0-4]\d)|(25[0-5])))$)/,
+    mac: /^(([0-9]|[A-F]){2}-){5}([0-9]|[A-F]){2}$/
+}
+
+const validate = (valueToValidate, type) => {
+    return Roles[type].test(valueToValidate);
+}
+
 export default validate;
